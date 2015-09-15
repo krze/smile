4f83496b7830201131c397fa40c164460e6b09ec
Ken Krzeminski <krze@tumblr.com>
2015-09-15 08:12:33 -0400

    Sliding snappy tray

--- smile/ViewController.swift
+++ smile/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var trayOriginalCenter: CGPoint!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    
+    
+    
+    @IBOutlet weak var trayView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        trayUp = CGPointMake(160, 470)
+        trayDown = CGPointMake(160, 634)
+        
+//        trayView.center = trayUp
+        println("Tray center on load is: \(trayView.center)")
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    
+    @IBAction func onTrayPanGesture(sender: UIPanGestureRecognizer) {
+        var panGestureRecognizer = sender
+        var point = panGestureRecognizer.locationInView(view)
+        var velocity = panGestureRecognizer.velocityInView(view)
+        var translation = panGestureRecognizer.translationInView(view)
+        var goingUp = Bool()
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            println("Gesture began at: \(point)")
+            trayOriginalCenter = trayView.center
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            println("Gesture changed at: \(point)")
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+            println("Current Tray Center Is: \(trayView.center)")
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            println("Gesture ended at: \(point)")
+            
+            if velocity.y < 0 {
+                goingUp = true
+            } else {
+                goingUp = false
+            }
+            if goingUp {
+                trayView.center = trayUp
+            } else {
+                trayView.center = trayDown
+            }
+        }
+    }
+    
 
 }
 
